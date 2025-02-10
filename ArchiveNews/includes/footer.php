712c850f593777ff25b9c7048dea4a802f82be8f
MaxZorin44456 <MaxZorin44456@gmail.com>
2025-02-10 23:35:06 +0000

    Add files via upload

---
+++ ArchiveNews/includes/footer.php
@@ -0,0 +1,42 @@
+<?php set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
+?>
+<html>
+<body>
+<!-- Tweet -->
+		<div id="tweet">
+			<div class="container">
+				<section>
+					<blockquote>&ldquo;Intuitive improvisation is the secret of genius.&rdquo;</blockquote>
+				</section>
+			</div>
+		</div>
+
+	<!-- Footer -->
+		<div id="footer">
+			<div class="container">
+				<section>
+					<header>
+						<h2>Get in touch</h2>
+						<span class="byline">You can contact me on the following sites:</span>
+					</header>
+					<ul class="contact">
+                        <li><a href="mailto:contactme@MaxZorin44456.com" class="fa fa-envelope"><span>Email</span></a><l>
+						<li><a href="http://max-zorin-44456.tumblr.com/" class="fa fa-tumblr"><span>Tumblr</span></a></li>
+                        <li><a href="https://twitter.com/MaxZorin44456/" class="fa fa-twitter"><span>Twitter</span></a><l>
+                        <li><a href="http://www.youtube.com/user/MaxZorin44456/" class="fa fa-youtube-play"><span>YouTube</span></a><li>
+                        <li><a href="http://www.planetminecraft.com/member/maxzorin44456/" class="fa fa-globe"><span>Planet Minecraft</span></a><l>
+                        <li><a href="https://www.facebook.com/MaxZorin44456/" class="fa fa-facebook"><span>Facebook</span></a><li>
+					</ul>
+				</section>
+			</div>
+		</div>
+
+	<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+            	<p><a href="../Sitemap">Sitemap</a></p>
+				Design: <a href="http://templated.co">TEMPLATED</a> • Copyright 2014-2018 • All Rights Reserved
+			</div>
+		</div>
+</body>
+</html>
