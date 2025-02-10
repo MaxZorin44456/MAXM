cdb2c51803080cf21243cc9c8a30fc781c8d8816
MaxZorin44456 <MaxZorin44456@gmail.com>
2025-02-10 22:48:53 +0000

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
+?>
+<html>
+<body>
+<!-- Header -->
+		<div id="header">
+			<div id="nav-wrapper"> 
+				<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="active"><a href="index">Home</a></li>
+                        <li><a href="Projects">My Projects</a></li>
+                        <li><a href="News">News</a></li>
+                        <li><a href="About">About</a></li>
+                        <li><a href="ToDoList">To-Do List</a></li>
+					</ul>
+				</nav>
+			</div>
+            </div>
+</body>
+</html>
